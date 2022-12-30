fb9b487531903010bb5151b69fa1b0874a7d531c
Ej Sadiarin <ejsadiarin@gmail.com>
2022-12-30 22:15:17 +0800

    Add project files.

---
+++ Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI_Practice.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
