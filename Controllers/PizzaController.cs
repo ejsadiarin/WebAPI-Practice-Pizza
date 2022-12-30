7bd0486a99e4bd0ff9a4adc34ad225cbf3d5de0a
Ej Sadiarin <ejsadiarin@gmail.com>
2022-12-30 23:00:29 +0800

    added POST, PUT, DELETE actions in PizzaController, which means Create, Update, Delete method has been added, respectfully

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -8,14 +8,10 @@ namespace WebAPI_Practice.Controllers
     [Route("[controller]")]
     public class PizzaController : ControllerBase
     {
-        public PizzaController()
-        {
-
-        }
-
         // GET all action
         [HttpGet]
         public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
         // GET id action
         [HttpGet("{id}")]
         public ActionResult<Pizza> Get(int id)
@@ -25,11 +21,47 @@ namespace WebAPI_Practice.Controllers
 
             return pizza;
         }
+
         // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            // returns IActionResult response
+            // IActionResult lets client know if request succeeded and provides ID of newly created pizza
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+
+        }
 
         // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            // updates pizza and returns result
+            // returns 204 NoContent HTTP status code if success, 404 BadRequest if not
+            if (id != pizza.Id) return BadRequest();
 
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null) return NotFound();
+
+            PizzaService.Update(pizza);
+            return NoContent();
+        }
+        
         // DELETE action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // deletes pizza by id and returns result
+            // returns 204 NoContent HTTP status code if success, 404 NotFound if not
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null) return NotFound();
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
 
     }
 }
